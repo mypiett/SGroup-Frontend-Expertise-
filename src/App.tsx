1f0e9421bda3e72388c59e3789a0524ce93e20e1
thanhtuyen <thanhtuyen27122005@gmail.com>
2025-10-08 16:08:39 +0700

    fix:fix login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LoginPage from "@/pages/login/ui/LoginPage"
+import DashBoardPage from "./pages/dashboard/DashBoardPage"
 
 
 function App() {
   return (
     <>
       <LoginPage/>
+      <DashBoardPage/>
     </>
   )
 }
